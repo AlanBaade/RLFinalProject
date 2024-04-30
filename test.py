6b9235d87e6566049dd31a42d47c7b3c5a217d6d
Linus Zheng <linus.zheng@warriorlife.net>
2024-04-30 00:58:52 -0500

    merge

--- test.py
+++ test.py
@@ -14,11 +14,17 @@ env = gym.make('SoccerEnv-v0')
 obs, _ = env.reset()
 done = False
 
-idx = 0
+
+i = 0
 while not done:
     action, _ = model.predict(obs)
     obs, reward, done, _, _ = env.step(action)
-    if idx % 1 == 0:
+    if (i % 10)==1:
         env.render()
-        time.sleep(0.1)
-    idx += 1
+    i += 1
+    print(reward)
+    print(i)
+
+
+
+
